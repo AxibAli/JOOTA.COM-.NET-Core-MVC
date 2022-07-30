163bcabd41995e65592e9c82b70cdf189e0052c0
Fasiha <Fasihasohail45@gmail.com>
2022-07-30 21:55:01 +0500

    Initial push by Fasiha (30/july/2022)

---
+++ Joota.com/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Joota.com.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
